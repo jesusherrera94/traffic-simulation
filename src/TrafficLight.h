51afd57647abf1b451bfcdd11eeee2420cf8415f
Jesus Herrera <jesusherrera1594@gmail.com>
2024-11-12 17:41:47 -0600

    start task FP.2 but complete task FP.3 in order to continue with task FP.2

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -10,18 +10,18 @@
 // forward declarations to avoid include cycle
 class Vehicle;
 
-
-// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
-// Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
-// Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
-// Also, there should be an std::condition_variable as well as an std::mutex as private members. 
-
 template <class T>
 class MessageQueue
 {
 public:
+    MessageQueue();
+    T receive();
+    void send(T &&msg);
 
 private:
+    std::deque<T> _queue;
+    std::condition_variable _condition;
+    std::mutex _mutex;
     
 };
 
